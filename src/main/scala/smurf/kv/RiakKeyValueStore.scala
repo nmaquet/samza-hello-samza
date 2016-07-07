619b5aeddcfd3adaad890bfb048f20045418eded
ouchxp <ouchxp.me@gmail.com>
2016-07-07 20:52:30 +1200

    Remove test app

--- src/main/scala/smurf/kv/RiakKeyValueStore.scala
+++ src/main/scala/smurf/kv/RiakKeyValueStore.scala
@@ -1,10 +1,9 @@
 package smurf.kv
 
-import java.{sql, util}
+import java.{util}
 
 import com.basho.riak.client.api.RiakClient
 import com.basho.riak.client.api.commands.kv.{DeleteValue, FetchValue, StoreValue}
-import com.basho.riak.client.core.operations.SearchOperation
 import com.basho.riak.client.core.query.{Location, Namespace, RiakObject}
 import com.basho.riak.client.core.util.BinaryValue
 import com.basho.riak.client.core.{RiakCluster, RiakNode}
@@ -77,11 +76,4 @@ class RiakKeyValueStore(
   }
 }
 
-object TestApp extends App {
-  val store = new RiakKeyValueStore("localhost", 32769, "profile")
-  // store.put("hello".getBytes, "world1".getBytes())
-  println(new String(store.get("hello".getBytes)))
-  println("done")
-  store.close()
-}
 
