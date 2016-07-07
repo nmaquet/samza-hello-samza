ecc699d587c41bb186af0aa7cb8fb85d1753d381
ouchxp <ouchxp.me@gmail.com>
2016-07-07 15:26:13 +1200

    Implement mysql keyvaluestore

--- src/main/scala/smurf/kv/MySQLKeyValueStore.scala
+++ src/main/scala/smurf/kv/MySQLKeyValueStore.scala
@@ -18,10 +18,29 @@ class MySQLKeyValueStore(
 
   val conn = sql.DriverManager.getConnection(s"jdbc:mysql://$url/$database", user, password)
 
-  override def range(k: Array[Byte], k1: Array[Byte]): KeyValueIterator[Array[Byte], Array[Byte]] = ???
+  override def range(from: Array[Byte], to: Array[Byte]): KeyValueIterator[Array[Byte], Array[Byte]] = {
+    val statement = conn.prepareStatement(s"SELECT `key`, `value` FROM `$table` WHERE `key` >= ? AND `key` < ?")
+    statement.setBytes(1, from)
+    statement.setBytes(2, to)
+    val rs = statement.executeQuery()
+
+    new KeyValueIterator[Array[Byte], Array[Byte]] {
+      override def close(): Unit = {
+        rs.close()
+        statement.close()
+      }
+
+      override def next(): Entry[Array[Byte], Array[Byte]] = new Entry[Array[Byte], Array[Byte]](rs.getBytes(1), rs.getBytes(2))
+
+      override def hasNext: Boolean = rs.next()
+    }
+  }
 
   override def get(k: Array[Byte]): Array[Byte] = {
-    ???
+    val statement = conn.prepareStatement(s"SELECT `value` FROM `$table` WHERE `key` = ?")
+    statement.setBytes(1, k)
+    val rs = statement.executeQuery()
+    rs.getBytes(1)
   }
 
   override def put(k: Array[Byte], v: Array[Byte]): Unit = {
@@ -31,7 +50,21 @@ class MySQLKeyValueStore(
     statement.executeUpdate()
   }
 
-  override def all(): KeyValueIterator[Array[Byte], Array[Byte]] = ???
+  override def all(): KeyValueIterator[Array[Byte], Array[Byte]] = {
+    val statement = conn.prepareStatement(s"SELECT `key`, `value` FROM `$table`")
+    val rs = statement.executeQuery()
+
+    new KeyValueIterator[Array[Byte], Array[Byte]] {
+      override def close(): Unit = {
+        rs.close()
+        statement.close()
+      }
+
+      override def next(): Entry[Array[Byte], Array[Byte]] = new Entry[Array[Byte], Array[Byte]](rs.getBytes(1), rs.getBytes(2))
+
+      override def hasNext: Boolean = rs.next()
+    }
+  }
 
   override def flush(): Unit = {
     // no-op
